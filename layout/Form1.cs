785323704c02a9dd68f288fac55dba766456bfca
hhelibebc <50080470+hhelibebc@users.noreply.github.com>
2020-11-22 11:25:13 +0800

    add  a layout folder/project
    folder "layout" contain two contexts.First is GUI Layout Toolbox named Layout.cs.The remaining files are examples of basic calculator based on Layout.cs

---
+++ layout/Form1.cs
@@ -0,0 +1,470 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace layout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Paint += Form1_Paint;
+            Resize += Form1_Resize;
+            MouseClick += Form1_MouseClick;
+
+            pen = new Pen(Color.Black);
+            MyLayout.Panel.output = new List<MyLayout.Panel.PARAM>();
+
+            Basic();
+            //Test1();
+            //Test2();
+            //Test3();
+            //Test4();
+            //Test5();
+            //Test6();
+        }
+        void Test1()
+        {
+            main = new MyLayout.HBox();
+            main.SetParam(null, 5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddVBox(5, 5);
+            main.AddGrid(5, 5);
+            main.SetHorizontal(new short[] { 100, -1, -2, -1, 100 });
+        }
+        void Test2()
+        {
+            main = new MyLayout.VBox();
+            main.SetParam(null, 5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddVBox(5, 5);
+            main.AddGrid(5, 5);
+            main.SetVertical(new short[] { 100, -1, -2, -1, 100 });
+        }
+        void Test3()
+        {
+
+            main = new MyLayout.Grid();
+            main.SetParam(null, 5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+
+            main.AddEmpty(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddEmpty(5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddEmpty(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.SetHorizontal(new short[] { 100, -1, -2, -1, 100 });
+            main.SetVertical(new short[] { 100, -1, -2, -1, 100 });
+        }
+        void Test4()
+        {
+            main = new MyLayout.VBox();
+            main.SetParam(null, 10, 10);
+
+            main.AddHBox(5, 5);
+            main.AddPanel(5, 5);
+            main.AddGrid(5, 3);
+            main.SetVertical(new short[] { 40, 120, -1 });
+
+            MyLayout.Panel p = main.children[0];
+            p.AddPanel(5, 5);
+            p.AddEmpty(5, 5);
+            p.AddPanel(5, 5);
+            p.SetHorizontal(new short[] { 100, -1, 100 });
+
+            MyLayout.Panel p2 = main.children[2];
+            p2.AddPanel(0, 0, 24);
+            p2.SetHorizontal(MyLayout.Panel.GetDefault(4));
+            p2.SetVertical(MyLayout.Panel.GetDefault(6));
+        }
+        void Test5()
+        {
+            main = new MyLayout.VBox();
+            main.SetParam(null, 5, 5);
+            main.AddHBox(3, 3);
+            main.AddHBox(3, 3);
+            main.AddPanel(3, 3);
+            main.AddHBox(3, 3);
+            main.AddHBox(3, 3);
+            main.AddHBox(3, 3);
+            main.AddGrid(3, 3);
+            main.SetVertical(new short[] { 40, 50, 120, -1, -1, 40, -7 });
+
+            MyLayout.Panel p = main.children[0];
+            p.AddPanel(0, 0);
+            p.AddEmpty(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.SetHorizontal(new short[] { 100, -1, 100, 100, 100 });
+
+            p = main.children[1];
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddEmpty(0, 0);
+            p.AddPanel(0, 0);
+            p.SetHorizontal(new short[] { 50, 120, -1, 50 });
+
+            p = main.children[3];
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddEmpty(0, 0);
+            p.SetHorizontal(new short[] { -1, -1, -4});
+
+            p = main.children[4];
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.SetHorizontal(MyLayout.Panel.GetDefault(6));
+
+            p = main.children[5];
+            p.AddPanel(0, 0);
+            p.AddPanel(0, 0);
+            p.AddEmpty(0, 0);
+            p.SetHorizontal(new short[] { 140, 120, -1 });
+
+            p = main.children[6];
+            p.AddPanel(0, 0, 35);
+            p.SetHorizontal(MyLayout.Panel.GetDefault(5));
+            p.SetVertical(MyLayout.Panel.GetDefault(7));
+        }
+        void Test6()
+        {
+            main = new MyLayout.HBox();
+            main.SetParam(null, 30, 10);
+
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.AddPanel(5, 5);
+            main.SetHorizontal(new short[] { 100, -1, -2, -1, 100 });
+        }
+        void Basic()
+        {
+            SuspendLayout();
+            Controls.AddRange(MyLayout.Panel.GetTextBoxes(1));
+            Controls.AddRange(MyLayout.Panel.GetButtons(24));
+            for(int i = 0; i < 24; i++)
+            {
+                Controls[i + 1].Text = names[i];
+                Controls[i + 1].Click += ButtonClick;
+            }
+            ResumeLayout(false);
+
+            main = new MyLayout.VBox();
+            main.rect.X = 10;
+            main.rect.Y = 10;
+            main.SetParam(null, 10, 5);
+
+            main.AddPanel(0, 0);
+            main.AddGrid(0, 3);
+            main.SetVertical(new short[] {120, -1 });
+            main.SetID(0, 0);
+
+            MyLayout.Panel p3 = main.children[1];
+            p3.AddPanel(3, 3, 24);
+            p3.SetHorizontal(MyLayout.Panel.GetDefault(4));
+            p3.SetVertical(MyLayout.Panel.GetDefault(6));
+            for (int i = 0; i < 24; i++)
+                p3.SetID((byte)i, (short)(i + 1));
+
+            Form1_Resize(null, null);
+            Initialized();
+        }
+        
+        void Initialized()
+        {
+            result = 0;
+            num = 0;
+            symbol = 0;
+            state = STATE.INITIAL;
+            tmp = "0";
+            isPos = true;
+            SetString();
+        }
+        void SetString()
+        {
+            Controls[0].Text = tmp;
+        }
+        void GetValue() 
+        {
+            double.TryParse(tmp, out num);
+        }
+        void InputN(int n)
+        {
+            if(tmp != "0" && state != STATE.SYM && state != STATE.FINAL)
+                tmp += n.ToString();
+            else
+                tmp = n.ToString();
+            SetString();
+            switch (state)
+            {
+                case STATE.INITIAL:
+                case STATE.SYM:
+                case STATE.FINAL:
+                    state = STATE.NUM;
+                    break;
+                case STATE.FLOAT_N:
+                    state = STATE.FLOAT_F;
+                    break;
+                default:break;
+            }
+        }
+        void InputSymbol(int n)
+        {
+            GetValue();
+            if (n != 0)
+            {
+                result = num;
+                isPos = true;
+            }
+            switch (symbol)
+            {
+                case 1: result += num; break;
+                case 2: result -= num; break;
+                case 3: result *= num; break;
+                case 4: result /= num; break;
+                default:break;
+            }
+
+            state = STATE.SYM;
+            symbol = (byte)n;
+        }
+        void InputOpt(int n)
+        {
+            GetValue();
+            if (num == 0 && n == 2)
+                tmp = errors[0];
+            else if (num < 0 && n == 4)
+                tmp = errors[1];
+            else
+            {
+                switch (n)
+                {
+                    case 1: result = num / 100; break;
+                    case 2: result = 1 / num; break;
+                    case 3: result = num * num; break;
+                    case 4: result = Math.Sqrt(num); break;
+                    default: break;
+                }
+                tmp = result.ToString();
+            }
+            SetString();
+            state = STATE.FINAL;
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            int idx = Controls.IndexOf((Control)sender);
+            switch (idx)
+            {
+                case 9:  InputN(7); break;
+                case 10: InputN(8); break;
+                case 11: InputN(9); break;
+                case 13: InputN(4); break;
+                case 14: InputN(5); break;
+                case 15: InputN(6); break;
+                case 17: InputN(1); break;
+                case 18: InputN(2); break;
+                case 19: InputN(3); break;
+                case 22: InputN(0); break;
+
+                case 1: InputOpt(1); break;
+                case 5: InputOpt(2); break;
+                case 6: InputOpt(3); break;
+                case 7: InputOpt(4); break;
+
+                case 8:
+                case 12:
+                case 16:
+                case 20:
+                    byte new_sym = (byte)((24 - idx) / 4);
+                    if (symbol != 0)
+                    {
+                        InputSymbol(0);
+                        symbol = new_sym;
+                        tmp = result.ToString();
+                        SetString();
+                    }
+                    else
+                        InputSymbol(new_sym);
+                    break;
+
+                case 2:           // clear error
+                    if (state == STATE.FINAL)
+                        Initialized();
+                    else
+                    {
+                        tmp = "0";
+                        num = 0;
+                        isPos = true;
+                        SetString();
+                        state = STATE.SYM;
+                    }
+                    break;
+                case 3:           // clear all
+                    if (state != STATE.INITIAL)
+                        Initialized();
+                    break;
+                case 4:           // backspace
+                    if (state != STATE.SYM && state != STATE.FINAL)
+                    {
+                        if (tmp.Length > 1)
+                        {
+                            if (tmp[tmp.Length - 1] == '.')
+                                state = STATE.NUM;
+                            tmp = tmp.Substring(0, tmp.Length - 1);
+                        }
+                        else if (tmp.Length == 1)
+                            tmp = "0";
+                        SetString();
+                    }
+                    break;
+                case 21:          // symbol
+                    if(tmp != "0")
+                    {
+                        if (isPos)
+                            tmp = "-" + tmp;
+                        else
+                            tmp = tmp.Substring(1);
+                        isPos = !isPos;
+                        SetString();
+                    }
+                    break;
+                case 23:          // point
+                    if(state != STATE.FLOAT_N && state != STATE.FLOAT_F)
+                    {
+                        if(state ==  STATE.SYM || state == STATE.FINAL)
+                            tmp = "0.";
+                        else
+                            tmp = tmp + ".";
+                        state = STATE.FLOAT_N;
+                        SetString();
+                    }
+                    break;
+                case 24:          // equal
+                    if(state != STATE.INITIAL && state != STATE.FINAL)
+                    {
+                        if (tmp == "0" && symbol == 4)
+                            tmp = errors[0];
+                        else
+                        {
+                            InputSymbol(0);
+                            tmp = result.ToString();
+                        }
+                        symbol = 0;
+                        state = STATE.FINAL;
+                        SetString();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //rect.X = e.X;
+            //rect.Y = e.Y;
+            //rect.Width = 100;
+            //rect.Height = 30;
+            //Form1_Paint(null, null);
+        }
+
+        void Adjust()
+        {
+            foreach(MyLayout.Panel.PARAM x in MyLayout.Panel.output)
+            {
+                Controls[x.id].Location = x.rect.Location;
+                Controls[x.id].Size = x.rect.Size;
+            }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            main.rect.Width = Width - 20;
+            main.rect.Height = Height - 50;
+            main.Update();
+            MyLayout.Panel.output.Clear();
+            main.Print();
+            Adjust();
+            //Invalidate(true);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //if (g != null)
+            //{
+            //    foreach (MyLayout.Panel.PARAM x in MyLayout.Panel.output)
+            //        g.DrawRectangle(pen, x.rect);
+            //}
+        }
+
+        Pen pen;
+        Graphics g;
+        MyLayout.Panel main;
+        STATE state;
+        double result = 0, num = 0;
+        byte symbol = 0;
+        bool isPos;
+        string tmp;
+        string[] names =
+        {
+            "%", "CE", "C", "退格",
+            "倒数", "平方", "平方根", "÷",
+            "7", "8", "9", "×",
+            "4", "5", "6", "-",
+            "1", "2", "3", "+",
+            "±", "0", "·", "=",
+        };
+        string[] errors =
+        {
+            "Can't divide by zero!",
+            "Can't get a root to a negative!"
+        };
+        enum STATE
+        {
+            INITIAL,
+            FLOAT_N,
+            FLOAT_F,
+            NUM,
+            SYM,
+            FINAL
+        };
+    }
+}
---
+++ layout/Layout.cs
@@ -0,0 +1,333 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MyLayout
+{
+    class Panel
+    {
+        public Panel()
+        {
+            id = -1;
+            type = TYPE.PANEL;
+            children = new List<Panel>();
+        }
+        public void AddPanel(byte p, byte s)
+        {
+            AddControl(TYPE.PANEL, p, s);
+        }
+        public void AddPanel(byte p, byte s, byte c)
+        {
+            for (int i = 0; i < c; i++)
+                AddControl(TYPE.PANEL, p, s);
+        }
+        public void AddHBox(byte p, byte s)
+        {
+            AddControl(TYPE.HBOX, p, s);
+        }
+        public void AddVBox(byte p, byte s)
+        {
+            AddControl(TYPE.VBOX, p, s);
+        }
+        public void AddGrid(byte p, byte s)
+        {
+            AddControl(TYPE.GRID, p, s);
+        }
+        public void AddEmpty(byte p, byte s)
+        {
+            AddControl(TYPE.EMPTY, p, s);
+        }
+        public void SetParam(Panel parent, byte p, byte s)
+        {
+            this.parent = parent;
+            padding = p;
+            spacing = s;
+        }
+        public void SetHorizontal(short[] arr) 
+        {
+            if (type == TYPE.HBOX)
+                ((HBox)this).horizontal = arr;
+            else if (type == TYPE.GRID)
+                ((Grid)this).horizontal = arr;
+            else
+                MessageBox.Show("不支持设置 horizontal 属性！");
+        }
+        public void SetVertical(short[] arr)
+        {
+            if (type == TYPE.VBOX)
+                ((VBox)this).vertical = arr;
+            else if (type == TYPE.GRID)
+                ((Grid)this).vertical = arr;
+            else
+                MessageBox.Show("不支持设置 vertical 属性！");
+        }
+        public void SetID(short id)
+        {
+            this.id = id;
+        }
+        public void SetID(byte target,short id)
+        {
+            children[target].id = id;
+        }
+        public void SetID(byte[] target,short id)
+        {
+            Panel p = GetControl(target);
+            if(p != null)
+                p.id = id;
+        }
+        public void Update()
+        {
+            if (children == null || children.Count == 0)
+                return;
+            byte v = (byte)(padding << 1), r = 1, c = 1;
+            switch (type)
+            {
+                //case TYPE.PANEL:
+                //    for(int i = 0; i < children.Count; i++)
+                //    {
+                //        children[i].rect.X = rect.X + padding;
+                //        children[i].rect.Y = rect.Y + padding;
+                //        children[i].rect.Width = rect.Width - v;
+                //        children[i].rect.Height = rect.Height - v;
+                //    }
+                //    break;
+                case TYPE.HBOX:
+                    for (int i = 0; i < children.Count; i++)
+                    {
+                        children[i].rect.Y = rect.Y + padding;
+                        children[i].rect.Height = rect.Height - v;
+                    }
+                    c = (byte)((HBox)this).horizontal.Length;
+                    DisposeWidth(1, c);
+                    DisposeLeft(1, c);
+                    break;
+                case TYPE.VBOX:
+                    for (int i = 0; i < children.Count; i++)
+                    {
+                        children[i].rect.X = rect.X + padding;
+                        children[i].rect.Width = rect.Width - v;
+                    }
+                    r = (byte)((VBox)this).vertical.Length;
+                    DisposeHeight(r, 1);
+                    DisposeTop(r, 1);
+                    break;
+                case TYPE.GRID:
+                    r = (byte)((Grid)this).vertical.Length;
+                    c = (byte)((Grid)this).horizontal.Length;
+                    DisposeWidth(r, c);
+                    DisposeLeft(r, c);
+                    DisposeHeight(r, c);
+                    DisposeTop(r, c);
+                    break;
+                default:break;
+            }
+            for (int i = 0; i < children.Count; i++)
+                children[i].Update();
+        }
+        public void Print()
+        {
+            if (type == TYPE.PANEL)
+            //if(type != TYPE.EMPTY)
+                output.Add(new PARAM(rect, id));
+            for (int i = 0; i < children.Count; i++)
+                children[i].Print();
+        }
+        static public short[] GetDefault(byte n)
+        {
+            short[] tmp = new short[n];
+            for (int i = 0; i < n; i++)
+                tmp[i] = -1;
+            return tmp;
+        }
+        static public TextBox[] GetTextBoxes(byte n)
+        {
+            TextBox[] tmp = new TextBox[n];
+            for (int i = 0; i < n; i++)
+            {
+                tmp[i] = new TextBox();
+                tmp[i].Multiline = true;
+                tmp[i].ReadOnly = true;
+            }
+            return tmp;
+        }
+        static public Label[] GetLabels(byte n)
+        {
+            Label[] tmp = new Label[n];
+            for (int i = 0; i < n; i++)
+            {
+                tmp[i] = new Label();
+                tmp[i].AutoSize = false;
+                tmp[i].BorderStyle = BorderStyle.FixedSingle;
+            }
+            return tmp;
+        }
+        static public Button[] GetButtons(byte n)
+        {
+            Button[] tmp = new Button[n];
+            for (int i = 0; i < n; i++)
+            {
+                tmp[i] = new Button();
+                tmp[i].AutoSize = false;
+            }
+            return tmp;
+        }
+        void DisposeLeft(byte r,byte c)
+        {
+            for(int i = 0; i < r; i++)
+            {
+                children[i * c].rect.X = rect.X + padding;
+                for (int j = 1; j < c; j++)
+                    children[i * c + j].rect.X = children[i * c + j - 1].rect.Right + spacing;
+            }
+        }
+        void DisposeTop(byte r, byte c)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                children[j].rect.Y = rect.Y + padding;
+                for (int i = 1; i < r; i++)
+                    children[i * c + j].rect.Y = children[(i - 1) * c + j].rect.Bottom + spacing;
+            }
+        }
+        void DisposeWidth(byte r, byte c)
+        {
+            short[] tmp;
+            if (type == TYPE.HBOX)
+                tmp = ((HBox)this).horizontal;
+            else
+                tmp = ((Grid)this).horizontal;
+            short pos = 0, neg = 0, remain = 0;
+            for(int i = 0; i < c; i++)
+            {
+                if (tmp[i] >= 0)
+                    pos += tmp[i];
+                else
+                    neg += tmp[i];
+            }
+            remain = (short)(rect.Width - pos - 2 * padding - (c - 1) * spacing);
+            for(int i = 0; i < r; i++)
+            {
+                for(int j = 0; j < c; j++)
+                {
+                    if (tmp[j] >= 0)
+                        children[i * c + j].rect.Width = tmp[j];
+                    else
+                        children[i * c + j].rect.Width = remain * tmp[j] / neg;
+                }
+            }
+        }
+        void DisposeHeight(byte r, byte c)
+        {
+            short[] tmp;
+            if (type == TYPE.VBOX)
+                tmp = ((VBox)this).vertical;
+            else
+                tmp = ((Grid)this).vertical;
+            short pos = 0, neg = 0, remain = 0;
+            for (int i = 0; i < r; i++)
+            {
+                if (tmp[i] >= 0)
+                    pos += tmp[i];
+                else
+                    neg += tmp[i];
+            }
+            remain = (short)(rect.Height - pos - 2 * padding - (r - 1) * spacing);
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (tmp[i] >= 0)
+                        children[i * c + j].rect.Height = tmp[i];
+                    else
+                        children[i * c + j].rect.Height = remain * tmp[i] / neg;
+                }
+            }
+        }
+        void AddControl(TYPE t, byte p, byte s)
+        {
+            switch (type)
+            {
+                case TYPE.EMPTY:
+                case TYPE.PANEL:
+                    MessageBox.Show("当前对象不支持添加子布局器！");
+                    return;
+                default:break;
+            }
+            switch (t)
+            {
+                case TYPE.EMPTY: children.Add(new Empty()); break;
+                case TYPE.PANEL: children.Add(new Panel()); break;
+                case TYPE.HBOX: children.Add(new HBox()); break;
+                case TYPE.VBOX: children.Add(new VBox()); break;
+                case TYPE.GRID: children.Add(new Grid()); break;
+                default: break;
+            }
+            children.Last().SetParam(this, p, s);
+        }
+        Panel GetControl(byte[] target)
+        {
+            Panel p = this;
+            int i;
+            for(i = 0; i < target.Length; i++)
+            {
+                if (target[i] < p.children.Count)
+                    p = p.children[target[i]];
+                else
+                    break;
+            }
+            if(i == target.Length)
+                return p;
+            return null;
+        }
+
+        public enum TYPE { 
+            PANEL,
+            HBOX,
+            VBOX,
+            GRID,
+            EMPTY
+        };
+        public struct PARAM
+        {
+            public PARAM(Rectangle r, short i)
+            {
+                rect = r;
+                id = i;
+            }
+            public Rectangle rect;
+            public short id;
+        };
+        public static List<PARAM> output;
+        public List<Panel> children;
+        public TYPE type;
+        public Rectangle rect;
+
+        short id;
+        Panel parent;
+        byte padding, spacing;
+    }
+    class HBox : Panel
+    {
+        public HBox() => type = TYPE.HBOX;
+
+        public short[] horizontal;
+    }
+    class VBox : Panel
+    {
+        public VBox() => type = TYPE.VBOX;
+
+        public short[] vertical;
+    }
+    class Grid : Panel
+    {
+        public Grid() => type = TYPE.GRID;
+
+        public short[] horizontal;
+        public short[] vertical;
+    }
+    class Empty : Panel
+    {
+        public Empty() => type = TYPE.EMPTY;
+    }
+}
